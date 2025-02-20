d592aeb18a1d00c357d131a74a1d5ce1af9cd991
MaylsonS <maylsons274@gmail.com>
2025-02-20 13:38:37 -0300

    Primeiro commit
    Adicionando a extensão do jupiter para conseguir exibir as tabelas e gerar os testes.

---
+++ leitor.py
@@ -0,0 +1,11 @@
+# from tabula import read_pdf
+
+# tabelas = read_pdf("boletim.pdf", pages="1")
+
+# for tabela in tabelas:
+#     print (tabela)
+
+
+# # print(tabelas)
+import sys
+print(sys.executable)
