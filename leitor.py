e5254c83b60fe6946b648b3c2595698711e13e23
MaylsonS <maylsons274@gmail.com>
2025-02-21 14:02:11 -0300

    Usando o a biblioteca camelot
    Adicionado o camelot para ler o arquivo, precisa-se baixar o ghostwriter e colocar no PATH para rodar o cod.

--- leitor.py
+++ leitor.py
@@ -1,11 +1,20 @@
-# from tabula import read_pdf
 
-# tabelas = read_pdf("boletim.pdf", pages="1")
+print("Pypdf")
+import pypdf
+reader = pypdf.PdfReader("relatorio.pdf")
+texto = "\n".join([page.extract_text() for page in reader.pages])
+print(texto)
 
-# for tabela in tabelas:
-#     print (tabela)
+print("_"*80)
+print("CAMELOT")
 
+import camelot
 
-# # print(tabelas)
-import sys
-print(sys.executable)
+# Tente com flavor="stream" se lattice falhar
+tabelas = camelot.read_pdf("relatorio.pdf", pages="1", flavor="stream")
+print(f"Quantidade de tabelas: {len(tabelas)}")
+
+# Iterar sobre as tabelas e exibir os dados
+for i, tabela in enumerate(tabelas):
+    print(f"\nTabela {i + 1}:")
+    print(tabela.df) 
