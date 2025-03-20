c30860d5e35b7795065f2cc4990fbf3b11aa5f14
MaylsonS <maylsons274@gmail.com>
2025-03-20 11:50:40 -0300

    Projeto Finalizado

--- leitor.py
+++ leitor.py
@@ -107,25 +107,3 @@ def subir_arquivo():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-# print("Pypdf")
-# import pypdf
-# reader = pypdf.PdfReader("relatorio2.pdf")
-# texto = "\n".join([page.extract_text() for page in reader.pages])
-# print(texto)
-
-# print("_"*80)
-# print("CAMELOT")
-
-# import camelot
-
-# # Tente com flavor="stream" se lattice falhar
-# tabelas = camelot.read_pdf("relatorio.pdf", pages="1", flavor="stream")
-# print(f"Quantidade de tabelas: {len(tabelas)}")
-
-# # Iterar sobre as tabelas e exibir os dados
-# for i, tabela in enumerate(tabelas):
-#     print(f"\nTabela {i + 1}:")
-#     print(tabela.df) 
-
